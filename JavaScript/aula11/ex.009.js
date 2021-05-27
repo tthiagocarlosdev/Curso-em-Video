7d8e01bccc13c5e40097f55db087ebea3fa658e8
tthiagocarlosdev <tthiagocarlos.dev@gmail.com>
2021-05-27 17:31:55 -0300

    finished class 11

---
+++ JavaScript/aula11/ex.009.js
@@ -0,0 +1,7 @@
+var país ='EUA'
+console.log(`Vivendo em ${país}`)
+if (país == 'Brasil') { // CONDICIONAL COMPOSTA!
+    console.log('Você é Brasileiro!')
+}else{
+    console.log('Você é Estrangeiro!')
+}
---
+++ JavaScript/aula11/ex008.js
@@ -0,0 +1,6 @@
+var velocidade = 60.1
+console.log(`A velocidade do seu carro é ${velocidade.toFixed(1)}km/h`)
+if (velocidade > 60){ // CONDIÇÃO SIMPLES!
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Diriga sempre usando cinto de segurança!`)
