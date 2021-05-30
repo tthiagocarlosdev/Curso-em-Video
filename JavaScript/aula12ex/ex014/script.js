f98a8ab6cf37cc110c62bef252e1aed3fd5fab58
tthiagocarlosdev <tthiagocarlos.dev@gmail.com>
2021-05-29 21:23:37 -0300

    exercise part 3

--- JavaScript/aula12ex/ex014/script.js
+++ JavaScript/aula12ex/ex014/script.js
@@ -2,20 +2,20 @@ function load() {
     var mensagem = window.document.getElementById('mensage')
     var image = window.document.getElementById('image')
     var data = new Date()
-    //var hora = data.getHours()
-    hora = 22
+    var hora = data.getHours()
+    //hora = 12
     mensagem.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
         //bom dia
-        image.src = 'morning2.png'
+        image.src = 'morning.png'
         document.body.style.background = '#c2c2c0'
     } else if (hora <= 18){
         // boa tarde
-        image.src = 'afternoon2.png'
+        image.src = 'afternoon.png'
         document.body.style.background = '#1b3576'
     }else{
         // boa noite
-        image.src = 'night2.png'
+        image.src = 'night.png'
         document.body.style.background = '#0f0f11'
     }
 }
---
+++ JavaScript/aula12ex/ex015/script.js
@@ -0,0 +1,8 @@
+var age = window.document.getElementById('age')
+var now = new Date()
+var year = now.getFullYear()
+function load() {
+    var actuallyAge = age - year
+        
+}
+
