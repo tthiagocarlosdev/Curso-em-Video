c228e50b34a3d8e7bedc85e94d012ce760e0d174
tthiagocarlosdev <tthiagocarlos.dev@gmail.com>
2021-06-03 11:31:59 -0300

    class 13 and 14

---
+++ JavaScript/aula13/ambiente.js
@@ -0,0 +1,23 @@
+computador = 1
+do{
+    console.log(`Passo ${computador}`)
+    computador++
+}while(computador <= 6)
+
+
+
+
+/*var computador = 1
+while (computador <= 3){
+    console.log((`Passo: ${computador}`))
+    computador++
+}*/
+
+/*
+console.log(('Tudo bem?'))
+console.log(('Tudo bem?'))
+console.log(('Tudo bem?'))
+console.log(('Tudo bem?'))
+console.log(('Tudo bem?'))
+console.log(('Tudo bem?'))
+*/
---
+++ JavaScript/aula14/ambiente.js
@@ -0,0 +1,5 @@
+console.log(`Vai començar....`)
+for(var computador = 1; computador <= 5; computador++){
+    console.log(`Passo ${computador}`)
+}
+console.log(`FIM!`)
