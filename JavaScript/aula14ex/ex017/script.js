6c6a2f47a44374ef7379a62b58d949dde5db1e8b
tthiagocarlosdev <tthiagocarlos.dev@gmail.com>
2021-06-05 19:50:18 -0300

    exercise 17

--- JavaScript/aula14ex/ex017/script.js
+++ JavaScript/aula14ex/ex017/script.js
@@ -1,1 +1,18 @@
-alert('Hello!')
+function gerarTabuada(){
+    var numero = document.getElementById('numero')
+    var resposta = document.getElementById('resposta')
+    if (numero.value.length == 0){
+        alert('Por favor, digite um número!')
+    }else{
+        numero = Number(numero.value)
+        let contador = 0
+        resposta.innerHTML = ``
+        do {
+            let item = document.createElement('option')
+            item.text += `${numero} x ${contador} = ${numero * contador}`
+            resposta.appendChild(item)
+            contador++
+        } while (contador <=9);
+    }
+    
+}
